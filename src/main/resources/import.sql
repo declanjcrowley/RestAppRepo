28b47fca56283f6d13c78e0b8203d4f4eda7be9d
declan.j.crowley <declan.j.crowley@mycit.ie>
2017-11-27 06:15:44 +0000

    table updates for items and persons

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,6 @@
 INSERT INTO artists VALUES (1,'Chadwick, Lynn','female'),(2,'Ono, Yoko','female'),(3,'Opie, Julian','male'),(4,'Etty, William','male'),(5,'Wallis, Henry','male');
 INSERT INTO movements VALUES (1,'Geometry of Fear'),(2,'Kinetic Art'),(3,'New British Sculpture'),(4,'Romanticism');
 INSERT INTO artist_movements VALUES (1,1),(1,2),(3,3),(4,4),(5,4);
+
+INSERT INTO items VALUES (1,'red',12,1.22),(2,'blue',12,1.23),(3,'green',12,1.24),(4,'yellow',12,1.25)
+INSERT INTO persons VALUES (1,'john','doe')
